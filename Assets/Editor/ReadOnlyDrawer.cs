879248ded2ca6c930d3d7479fc8e66bfaf91ba64
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-04-22 12:08:59 +0800

    added documentation for readonly attribute

--- Assets/Editor/ReadOnlyDrawer.cs
+++ Assets/Editor/ReadOnlyDrawer.cs
@@ -4,6 +4,9 @@ using UnityEditor;
 
 namespace DungeonGenerator.Editor
 {
+    /// <summary>
+    /// Draws the field with the ReadOnly attribute to the inspector.
+    /// </summary>
     [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
     public class ReadOnlyDrawer : PropertyDrawer
     {
@@ -12,9 +15,7 @@ namespace DungeonGenerator.Editor
             return EditorGUI.GetPropertyHeight(property, label, true);
         }
  
-        public override void OnGUI(Rect position,
-            SerializedProperty property,
-            GUIContent label)
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             GUI.enabled = false;
             EditorGUI.PropertyField(position, property, label, true);
--- Assets/Scripts/Extensions/ReadOnlyAttribute.cs
+++ Assets/Scripts/Extensions/ReadOnlyAttribute.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 namespace DungeonGenerator.Extensions
 {
+    /// <summary>
+    /// Fields with the ReadOnly attribute will not be modified in the inspector.
+    /// </summary>
+    /// <remarks>
+    /// Referenced from: http://answers.unity.com/answers/801283/view.html
+    /// </remarks>
     public class ReadOnlyAttribute : PropertyAttribute
     {
         
